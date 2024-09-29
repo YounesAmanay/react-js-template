0d21223d6969a8463f28f818d82997c22ebcd578
amanay younes <younesamanay@gmail.coms>
2024-09-29 13:58:25 +0100

    added chadcn/ui

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,20 @@ import './App.css';
 
 import React from 'react';
 
+import { Button } from './components/ui/button';
+
 function App() {
-   return <h1>Hello World</h1>;
+   return (
+      <div className="h-screen w-full flex justify-center items-center gap-8 bg-slate-300">
+         <Button>hello</Button>
+         <Button variant="outline">hello</Button>
+         <Button variant="link">hello</Button>
+         <Button variant="ghost">hello</Button>
+         <Button variant="destructive">hello</Button>
+         <Button variant="secondary">hello</Button>
+         <Button variant="primary">hello</Button>
+      </div>
+   );
 }
 
 export default App;
--- vite.config.js
+++ vite.config.js
@@ -10,31 +10,26 @@ const PORT = 3031;
 const env = loadEnv('all', process.cwd());
 
 export default defineConfig({
-  // base: env.VITE_BASE_PATH,
-  plugins: [
-    react(),
-    checker({
-      eslint: {
-        lintCommand: 'eslint "./src/**/*.{js,jsx,ts,tsx}"',
+   // base: env.VITE_BASE_PATH,
+   plugins: [
+      react(),
+      checker({
+         eslint: {
+            lintCommand: 'eslint "./src/**/*.{js,jsx,ts,tsx}"',
+         },
+         overlay: {
+            position: 'tl',
+            initialIsOpen: false,
+         },
+      }),
+   ],
+   resolve: {
+      alias: {
+         '@': path.resolve(__dirname, './src'),
+         '~': path.resolve(__dirname, './node_modules'),
+         'src': path.resolve(__dirname, './src'),
       },
-      overlay: {
-        position: 'tl',
-        initialIsOpen: false,
-      },
-    }),
-  ],
-  resolve: {
-    alias: [
-      {
-        find: /^~(.+)/,
-        replacement: path.join(process.cwd(), 'node_modules/$1'),
-      },
-      {
-        find: /^src(.+)/,
-        replacement: path.join(process.cwd(), 'src/$1'),
-      },
-    ],
-  },
-  server: { port: PORT, host: true },
-  preview: { port: PORT, host: true },
+   },
+   server: { port: PORT, host: true },
+   preview: { port: PORT, host: true },
 });
